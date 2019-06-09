5d23d5f5807831c2f4ca4d792908ae1a5ee627bc
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-06-09 09:02:10 -0500

    Working on path for images in serviceworker.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -3,12 +3,12 @@
 const CACHE = "pwabuilder-precache";
 const precacheFiles = [
     "index.html",
-    "/assets/images/O2_44.png",
-    "/assets/images/O2_76.png",
-    "/assets/images/O2_152.png",
-    "/assets/images/O2_512.png",
-    "/assets/images/O2_1024.png",
-    "/assets/images/O2_1240x600.png",
+    "assets/images/O2_44.png",
+    "assets/images/O2_76.png",
+    "assets/images/O2_152.png",
+    "assets/images/O2_512.png",
+    "assets/images/O2_1024.png",
+    "assets/images/O2_1240x600.png",
 ];
 
 self.addEventListener("install", function (event) {
